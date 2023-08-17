534cadddec02a4251ab9939f082174203b0e17b8
DYagmur <30656517+DYagmur@users.noreply.github.com>
2023-08-18 00:27:51 +0300

    html structure

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
     let currentPosition = -4.86;
+    let player1; // Değişkenleri tanımladık, fakat henüz değer atamadık
+    let player2;
 
     $(".spin").click(function() {
-        // Your roulette spin logic here
-        // Update the wheel rotation and display result
+        // Roulette spin mantığını burada işleyin
+        // Tekerlek dönüşünü güncelleyin ve sonucu görüntüleyin
     });
 
-    // Function to change player names
     function editNames() {
-        var player1 = prompt("Change Player1 name");
-        var player2 = prompt("Change player2 name");
+        player1 = prompt("Change Player1 name"); // Değerleri burada atıyoruz
+        player2 = prompt("Change player2 name");
 
         $("p.Player1").html(player1);
         $("p.Player2").html(player2);
     }
 
-    // Function to roll the dice
     function rollTheDice() {
         var randomNumber1 = Math.floor(Math.random() * 6) + 1;
         var randomNumber2 = Math.floor(Math.random() * 6) + 1;
@@ -37,12 +37,10 @@ $(document).ready(function() {
         $("h1").html(result);
     }
 
-    // Attach the editNames function to the button click event
     $("#editNamesBtn").click(function() {
         editNames();
     });
 
-    // Attach the rollTheDice function to the button click event
     $("#dice-btn").click(function() {
         rollTheDice();
     });
